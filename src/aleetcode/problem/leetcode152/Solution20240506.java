1143a956724a8a81a81804142075b3371e07488d
zhangeaky <zyk090104@163.com>
2024-05-06 23:27:35 +0800

    明天上班。今天先到这啦。。 明天也要坚持刷！！！

--- src/aleetcode/problem/leetcode152/Solution20240506.java
+++ src/aleetcode/problem/leetcode152/Solution20240506.java
@@ -19,18 +19,62 @@ public class Solution20240506 {
 
         // dp数组的含义 下标为i的数前，连续最大乘积是dp[i]
         int[] dp = new int[nums.length+1];
+        boolean continueFlag = true;
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], Math.max(nums[1], nums[0]*nums[1]));
+        if (nums[0]*nums[1] > nums[1] && nums[0]*nums[1] > nums[0]) {
+            dp[1] = nums[0]*nums[1];
+            continueFlag = true;
+        }
+
+        if (nums[1] > nums[0] && nums[1]>nums[1]*nums[0]) {
+            dp[1] = nums[0]*nums[1];
+            continueFlag = true;
+        }
+
+        if (nums[0] > nums[1] && nums[0]>nums[1]*nums[0]) {
+            dp[1] = nums[0];
+            continueFlag = false;
+        }
 
         for (int i=2; i<nums.length; i++) {
-            dp[i] = Math.max(dp[i-1]*nums[i], Math.max(dp[i-1], nums[i]));
+            int valueOne = dp[i-1];
+            int valueTwo = dp[i-1]*nums[i];
+            int valueThree = nums[i];
+
+            if (continueFlag) {
+                if (valueThree >= valueTwo && valueThree >= valueOne) {
+                    dp[i] = valueThree;
+                }
+
+                if (valueTwo >= valueThree && valueTwo >= valueOne) {
+                    dp[i] = dp[i-1]*nums[i];
+                }
+
+                if (valueOne >= valueTwo && valueOne >= valueThree) {
+                    dp[i] = nums[i];
+                    continueFlag = false;
+                }
+
+            } else {
+
+                if (valueThree >= valueOne) {
+                    dp[i] = valueThree;
+                    continueFlag = true;
+                }
+
+                if (valueThree <= valueOne) {
+                    dp[i] = valueOne;
+                    continueFlag = false;
+                }
+            }
         }
 
         return dp[nums.length-1];
     }
 
     public static void main(String[] args) {
-
+        Solution20240506 ss = new Solution20240506();
+        System.out.println(ss.maxProduct(new int[]{2, 3, -2, 4}));
     }
 
 }
