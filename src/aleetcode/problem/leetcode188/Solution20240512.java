707f5d01317952841199824ea218a3f0aa62d333
zhangeaky <zyk090104@163.com>
2024-05-15 01:06:26 +0800

    股票完结

--- src/aleetcode/problem/leetcode188/Solution20240512.java
+++ src/aleetcode/problem/leetcode188/Solution20240512.java
@@ -3,6 +3,8 @@ package aleetcode.problem.leetcode188;
 import aleetcode.problem.instrument.Solution;
 import aleetcode.problem.instrument.SolutionMethod;
 
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iv/description/">买股票的最佳时机IV</a>
  */
@@ -12,16 +14,46 @@ public class Solution20240512 {
     @SolutionMethod
     public int maxProfit(int k, int[] prices) {
 
-        // dp 数组的定义
-        int[][] dp = new int[prices.length][2*k];
+        // 需要的状态细分, k==3为例:
+        // 0第0次不持有
+        // 1第1次持有
+        // 2第1次不持有
+        // 3第2次持有
+        // 4第2次不持有
+        // 5第3次持有
+        // 6第3次不持有
+        // 需要7中状态来描述 所有的情况
 
-        for (int i=0; i<prices.length; i++) {
-            // 不持有股票的收益
-            dp[i][0] = 0;
+        // dp 数组的定义
+        int[][] dp = new int[prices.length][2*k+1];
+        dp[0][0] = 0;
+        // dp 数组的初始化
+        for (int j=1; j<=k; j++) {
+            dp[0][2*j-1] = -prices[0];
         }
 
+        for (int i=1; i<prices.length; i++) {
+            dp[i][0] = dp[i-1][0];
+            for (int j=1; j<=k; j++) {
+                // 第j次持有
+                //   -延续前面的
+                //   -今天买入，前面是第j-1次不持有
+                dp[i][j*2-1] = Math.max(dp[i-1][j*2-1], dp[i-1][j*2-2] - prices[i]);
 
-        return 0;
+                // 第j次不持有
+                //  延续前一天状态
+                //  今天我卖出,前一天是j-1次持有
+                dp[i][j*2] = Math.max(dp[i-1][j*2], dp[i-1][j*2-1] + prices[i]);
+            }
+        }
+
+        // TODO: 2024/5/15 这个逻辑怎么确认 为甚么是这个 ？
+        return dp[prices.length-1][2*k];
+    }
+
+    public static void main(String[] args) {
+        Solution20240512 ss = new Solution20240512();
+        System.out.println(ss.maxProfit(2, new int[]{2, 4, 1}));
     }
 
 }
--- src/aleetcode/problem/leetcode714/Solution20240513.java
+++ src/aleetcode/problem/leetcode714/Solution20240513.java
@@ -13,38 +13,33 @@ public class Solution20240513 {
     @SolutionMethod(description = "")
     public int maxProfit(int[] prices, int fee) {
 
-
-        //状态定义 因为手续费的扣减 和卖出这个状态强相关。所以需要这个状态来。
         // 0 不持有
         // 1 持有股票
-        // 2 卖出日
-        int[][] dp = new int[prices.length][3];
+        int[][] dp = new int[prices.length][2];
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
-        dp[0][2] = -fee;
 
         for (int i=1; i<prices.length; i++) {
 
-            // 第i日 不持有,且不是卖出
+            // 第i日 不持有
             // - 与前一日保持一致
-            // - 前一日卖出
-
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][2]);
+            // - 今日卖出
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i] - fee);
 
             // 第i日 持有股票
             // - 与前一日保持一致
-            dp[i][1] = dp[i-1][1];
-
-
-            // 第i日 卖出日
-
-
-
+            // - 今日买入
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
         }
 
+        return Math.max(dp[prices.length-1][0], dp[prices.length-1][1]);
+    }
 
+    public static void main(String[] args) {
 
-
+        Solution20240513 ss = new Solution20240513();
+        System.out.println(ss.maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        System.out.println(ss.maxProfit(new int[]{1,3,7,5,10,3}, 3));
 
     }
 }
